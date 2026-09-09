220392d9925cde83e1f19911e6887cbc36a059a0
Leonardo de Brito <leonardode.b.c@gmail.com>
2026-09-08 22:07:16 -0300

    created component and page for user creation

--- src/utils/generate-hashed-password.ts
+++ src/utils/generate-hashed-password.ts
@@ -1,7 +1,7 @@
 import { hashPassword } from '@/lib/login/manage-login';
 
 (async () => {
-  const myPass = '';
+  const myPass = '123456';
   const hashOfMyPassToBase64 = await hashPassword(myPass);
 
   console.log({ hashOfMyPassToBase64 });
