39848e2143ed0f31a27366e0291afc0d8a108184
Leonardo de Brito <leonardode.b.c@gmail.com>
2026-09-09 21:39:09 -0300

    created create-user-action and added useActionState at CreateUserForm

---
+++ src/actions/user/create-user-action.ts
@@ -0,0 +1,47 @@
+'use server';
+
+import {
+  CreateUserSchema,
+  PublicUserDto,
+  PublicUserSchema,
+} from '@/lib/user/schemas';
+import { asyncDelay } from '@/utils/async-delay';
+import { getZodErrorMessages } from '@/utils/get-zod-error-messages';
+
+type CreateUserActionState = {
+  user: PublicUserDto;
+  errors: string[];
+  success: boolean;
+};
+
+export async function createUserAction(
+  state: CreateUserActionState,
+  formData: FormData,
+): Promise<CreateUserActionState> {
+  await asyncDelay(2000);
+
+  if (!(formData instanceof FormData)) {
+    return {
+      user: state.user,
+      errors: ['Dados inválidos'],
+      success: false,
+    };
+  }
+
+  const formObj = Object.fromEntries(formData.entries());
+  const parsedFormData = CreateUserSchema.safeParse(formObj);
+
+  if (!parsedFormData.success) {
+    return {
+      user: PublicUserSchema.parse(formObj),
+      errors: getZodErrorMessages(parsedFormData.error.format()),
+      success: false,
+    };
+  }
+
+  return {
+    user: state.user,
+    errors: [],
+    success: true,
+  };
+}
