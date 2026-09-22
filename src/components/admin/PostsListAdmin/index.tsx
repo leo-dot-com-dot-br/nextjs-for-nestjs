4b9f1ea0b7398bd7295429ff6ffbdbf0fb6fe8cc
Leonardo de Brito <leonardode.b.c@gmail.com>
2026-09-21 21:54:16 -0300

    added new model for posts comming from nestJS and new queries for posts list

--- src/components/admin/PostsListAdmin/index.tsx
+++ src/components/admin/PostsListAdmin/index.tsx
@@ -1,14 +1,25 @@
-import { findAllPostAdmin } from '@/lib/post/queries/admin';
+import { findAllPostFromApiAdmin } from '@/lib/post/queries/admin';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { DeletePostButton } from '../DeletePostButton';
 import ErrorMessage from '../../ErrorMessage';
 
 export default async function PostsListAdmin() {
-  const posts = await findAllPostAdmin();
+  const postsRes = await findAllPostFromApiAdmin();
 
+  if (!postsRes.success) {
+    console.log(postsRes.errors);
+    return (
+      <ErrorMessage
+        contentTitle='Ei...!'
+        content='Tente fazer login novamente'
+      />
+    );
+  }
+
+  const posts = postsRes.data;
   if (posts.length <= 0)
-    return <ErrorMessage contentTitle='Ei...!' content='Bora criar um post!' />;
+    return <ErrorMessage contentTitle='Ei' content='Bora criar um post!' />;
 
   return (
     <div className='mb-16'>
